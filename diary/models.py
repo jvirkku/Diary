e703a99af95b9c8cc133aaf548a302e90add2cc7
SirBearington <karl.bjorn@edu.turkuamk.fi>
2024-10-30 13:29:13 +0200

    Created Category model & migrated it to the database

--- diary/models.py
+++ diary/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 
+class Category(models.Model):
+    """A category that the user can asign to notes"""
+    COLOUR_CHOICES = [
+        ('#FF0000', 'Red'),
+        ('#00FF00', 'Green'),
+        ('#0000FF', 'Blue'),
+        ('#FFFF00', 'Yellow'),
+        ('#FF00FF', 'Magenta'),
+        ('#00FFFF', 'Cyan'),
+        ('#000000', 'Black'),
+        ('#FFFFFF', 'White'),
+    ]
 
+    category_name = models.CharField(max_length=100)
+    category_color = models.CharField(max_length=7, choices=COLOUR_CHOICES, default='#FFFFFF')  #default to white
+
+    class Meta:
+        verbose_name_plural = 'Categories'  # Plural name
+
+    def __str__(self) -> str:
+        return str(self.category_name)
+    
 class Note(models.Model):
     """A diary note added by the user"""
     IMPORTANCE_CHOICES = [
@@ -14,11 +35,13 @@ class Note(models.Model):
     public=models.BooleanField(default=False)#by default this is not chosen
     date_added=models.DateTimeField(auto_now_add=True) #adds date automatically to new reviews
     date_modified=models.DateTimeField(auto_now=True) #timestamp if review is modified
-    # category=models.ForeignKey(Category, on_delete=models.CASCADE, default=1)#connects reviews to categories in database
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)  # Allows null category = a note without a category
+    #category=models.ForeignKey(Category, on_delete=models.CASCADE, default=1)#connects reviews to categories in database
     #                                                      #if a category is deleted, so are the notes
 
 
     def __str__(self) -> str:
         '''Return a string representation of the model.'''
-        return self.my_note  #returns the note as text
-    
+        # Check if category is assigned to avoid "NoneType" errors
+        category_name = self.category.category_name if self.category else "No Category"
+        return f"{category_name}: {self.my_note}"  # Format as a single string
