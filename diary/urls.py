307febfc9f0221d84f6cc67489d892185efeb6b7
SirBearington <karl.bjorn@edu.turkuamk.fi>
2024-11-01 15:27:19 +0200

    "categories" and "category" pages

--- diary/urls.py
+++ diary/urls.py
@@ -7,6 +7,10 @@ app_name = 'diary'
 urlpatterns = [
     #homepage
     path('', views.index, name='index'),
+    #Page that shows all created categories
+    path('category_list/', views.category_list, name='category_list'),
+    #Page for a specific category and all its notes
+    path('category/<int:category_id>/', views.category, name='category'),
     # Listing all notes
-    path('notes/', views.all_notes, name='all_notes')
+    path('notes/', views.all_notes, name='all_notes'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -9,6 +9,17 @@ def index(request):
     """Index page for diary"""
     return render(request, 'diary/index.html')
 
+def category_list(request):
+    """Categories page that shows all categories"""
+    category_list = Category.objects.all()
+    return render(request, 'diary/category_list.html', {'categories': category_list})
+
+def category(request, category_id):
+    """Category page that shows one category and all of its notes"""
+    category = Category.objects.get(id=category_id)
+    notes = Note.objects.filter(category=category)
+    return render(request, 'diary/category.html', {'category': category, 'notes': notes})
+
 def all_notes(request):
     """ Listing all available notes on the /notes page """
     notes = Note.objects.order_by('date_added')
