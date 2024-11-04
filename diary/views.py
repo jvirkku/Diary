829c2af63ffb3e971781bd43b633cca4e288370d
Johanna Virkkunen <johanna.virkkunen@edu.turkuamk.fi>
2024-11-04 14:45:25 +0200

    changed index views

--- diary/views.py
+++ diary/views.py
@@ -7,7 +7,10 @@ from .forms import CategoryForm, NoteForm
 # Create your views here.
 def index(request):
     """Index page for diary"""
-    return render(request, 'diary/index.html')
+    notes = Note.objects.order_by('date_added')
+    # Dictionary containing all the notes. Keys - used in the template to access data. Values - data we send to the template.
+    context = {'notes' : notes} 
+    return render(request, 'diary/index.html', context)
 
 def category_list(request):
     """Categories page that shows all categories"""
