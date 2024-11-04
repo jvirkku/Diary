0a2d50306fa04e6121284613eea147a6efd70539
Johanna Virkkunen <johanna.virkkunen@edu.turkuamk.fi>
2024-11-04 14:49:11 +0200

    public notes are visible in index if not logged in

--- diary/views.py
+++ diary/views.py
@@ -7,7 +7,7 @@ from .forms import CategoryForm, NoteForm
 # Create your views here.
 def index(request):
     """Index page for diary"""
-    notes = Note.objects.order_by('date_added')
+    notes = Note.objects.filter(public=True).order_by('date_added') #if not logged in, public notes are visible
     # Dictionary containing all the notes. Keys - used in the template to access data. Values - data we send to the template.
     context = {'notes' : notes} 
     return render(request, 'diary/index.html', context)
