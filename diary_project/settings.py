beb96341a8256bd06ef8348d86b4b94e64b16802
Zukos <zukosek333@gmail.com>
2024-11-06 12:34:34 +0200

    Changed notes colours to better ones. Some more styles and adjustments to notes.

--- diary_project/settings.py
+++ diary_project/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / "diary/static",  # Adjust this to point to your static directory if needed
 ]
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
